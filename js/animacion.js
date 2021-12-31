ded2ae20c736b3ce8094e2b2e7622bf776cb31c9
Ruben-Jaramillo <tiburones.tk@gmail.com>
2021-12-31 07:06:31 -0500

    Se termino de animar el arbol usando vanilla js

--- js/animacion.js
+++ js/animacion.js
@@ -1,13 +1,24 @@
-let hojas = "*";
+let hojas = "";
 [10,2].forEach(row => {
-    Array(row).fill('').forEach((v,i) => {
+    new Array(row).fill('').forEach((v,i) => {
         hojas += ([
-            ...Array(9-i).fill("<span>-</span>"),
+            ...Array(9-i).fill("<span> </span>"),
             ...Array(1+i*2).fill("<span class='rojo'>*</span>"),
-            ...Array(9-i).fill("<span>-</span>"),
+            ...Array(9-i).fill("<span> </span>"),
 
         ].join(''))
-
+        hojas += "<br>"
     })
 })
 document.getElementById("arbol").innerHTML = hojas;
+let animacion = document.querySelectorAll(".rojo");
+animacion.innerHTML = hojas;
+function animar() {
+    for (let i = 0; i < animacion.length; i++) {
+        let tiempo = i/20 +1;
+        animacion[i].style.animation = "colores "+tiempo+"s infinite"
+    }
+}
+
+window.addEventListener('load',animar)
+
